d94e8baaa4043c9f44af61bc8e6b976991cab7a7
s.ditaranto <simodita99@gmail.com>
2024-01-26 15:35:53 +0100

    stile aggiunto, esercizio finito

--- js/script.js
+++ js/script.js
@@ -16,32 +16,17 @@
 const userDistance = prompt("Inserisci la distanza in km che vuoi percorrere");
 const userAge = prompt("Inserisci la tua età");
 
-// test
-// const userDistance = 133;
-// const userAge = 96;
-
 let ticketPrice = userDistance * 0.21;
-// console.log(ticketPrice);
 
-// controllo età dell'utente per vedere se si possono applicare degli sconti
-// if(userAge <= 17 || userAge >= 65) {
-//     if(userAge <= 17) {
-//         ticketPrice = ticketPrice - ((ticketPrice / 100) * 20);
-//         console.log(ticketPrice);
-//     } else {
-//         ticketPrice = ticketPrice - ((ticketPrice / 100) * 40);
-//         console.log(ticketPrice);
-//     }
-// }
-
+// controllo se posso effettuare lo sconto
 if(userAge <= 17) {
+    // se l'utente è minorenne
     ticketPrice = ticketPrice - ((ticketPrice / 100) * 20);
-    // document.getElementById("price").innerHTML = ticketPrice;
 } else if(userAge >= 65){
+    // se l'utente è over 65
     ticketPrice = ticketPrice - ((ticketPrice / 100) * 40);
-    // document.getElementById("price").innerHTML = ticketPrice;
 }
 
-document.getElementById("price").innerHTML = `${ticketPrice.toFixed(2)} €`;
+document.getElementById("price").innerHTML = `Il prezzo del tuo biglietto è ${ticketPrice.toFixed(2)} €`;
 
 
