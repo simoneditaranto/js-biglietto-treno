9b74455c4bafee13af749ade19a5b5a348e2b038
s.ditaranto <simodita99@gmail.com>
2024-01-29 13:19:16 +0100

    snack-1, controlli e stile aggiunto

---
+++ snack/snack-3/snack-js/script.js
@@ -0,0 +1,1 @@
+// Scrivi un programma che simula il gioco di "Carta, forbice, sasso". Chiedi all'utente di fare una scelta e confrontala con una scelta casuale del computer. Determina il vincitore o se c'è un pareggio
--- snack/snack_1/snack-js/script.js
+++ snack/snack_1/snack-js/script.js
@@ -1,11 +1,19 @@
 // Crea un programma che chiede all'utente una temperatura in gradi Celsius e la converte in Fahrenheit, tramite una ricerca su internet potrete trovare la formula corretta per effettuare la conversione. Visualizza la temperatura convertita.
 
-const celsius = prompt("Inserisci la temperatura in gradi Celsius");
+const celsius = prompt("Inserisci l temperatura in gradi Celsius");
 console.log(celsius);
 
 const fahr = (celsius * 1.8) + 32;
 console.log(fahr);
 
+if(isNaN(fahr)) {
 
-document.getElementById("g_celsius").innerHTML = `Hai inserito ${celsius}°`;
-document.getElementById("conversion").innerHTML = `La temperatura in farh è ${fahr}`;
+    document.getElementById("error").innerHTML = `Non hai inserito un valore corretto, ricarica la pagina`;
+    document.getElementById("error_fahr").innerHTML = `Impossibile effettuare il calcolo`;    
+
+} else{
+
+    document.getElementById("g_celsius").innerHTML = `${celsius}°`;
+    document.getElementById("conversion").innerHTML = `${fahr}`;
+
+}
